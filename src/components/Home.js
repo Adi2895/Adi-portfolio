f72cc7d360f6bc8c403323fc6b0ace62159472b1
Adi2895 <jangraaditya11@gmail.com>
2023-06-06 12:53:04 +0530

    change the intro part a little bit more

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,17 +9,19 @@ export default function Home() {
         <section >
         <div className="container " style={{marginTop:200, marginBottom:50}}>
             <div className="conatiner" >
-                <h1 className='myName' style={{fontSize:"3.8rem"}}>  <span className="hii"> hello</span>, there <br></br>I am Aditya</h1>
+                <h1 className='myName' style={{fontSize:"3.8rem"}}>  <span className="hii"> Hii</span>, there <br></br>I am Aditya</h1>
                 <h1 style={{fontSize:"3.8rem" , fontFamily:'Alkatra'}}>Full stack Web Developer </h1>
                 <p className='container content-justify' style={{fontSize:"1.4rem", textAlign:"left" }}>
                 I'm a tech enthusiast with a strong passion for coding. I find great joy and fulfillment in
                  exploring the vast and ever-evolving world of technology. 
                  With a positive attitude, I approach challenges with enthusiasm and an eagerness to learn and grow.
-                 
+                 <br/>
+                 <br/>
                  Coding has become more than just a skill for me; it's a genuine passion. I enjoy diving into programming languages, 
                  developing software, and solving complex problems using code. 
                  The ability to create something from scratch and witness it come 
                  to life through programming is truly fascinating to me.
+                 <br/>
                  <br/>
                  Having a keen interest in the tech world, I stay up to date with the latest trends, 
                  advancements, and innovations in the industry.
